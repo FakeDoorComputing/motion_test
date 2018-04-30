b9a9d4b74f5b23e82068fd4ba8f9d5640e4c7d5e
FakeDoorComputing <32259974+FakeDoorComputing@users.noreply.github.com>
2018-04-30 19:44:50 +0100

    Add files via upload

--- motion.js
+++ motion.js
@@ -1,16 +1,18 @@
 locOrientation = screen.lockOrientation || screen.mozLockOrientation || screen.msLockOrientation || screen.orientation.lock;
 locOrientation('portrait');
 window.addEventListener("deviceorientation", get_orientation, true);
-var sen_x, sen_y, sen_z, sen_a;
+var sen_y, sen_z, st_y, st_z, start=true;
 
 function get_orientation(event){
-  sen_x=event.alpha;
   sen_y=event.beta;
   sen_z=event.gamma;
-  sen_a=event.absolute;
-  $("#x_val").text(sen_x);
-  $("#y_val").text(sen_y);
-  $("#z_val").text(sen_z);
-  $("#error").text(sen_a);
-
+  if(start){
+    st_y=sen_y;
+    st_z=sen_z;
+    $("#y_start").text("y start = "+st_y);
+    $("#z_start").text("z start = "+st_z);
+    start=false;
+  }
+  $("#y_val").text(st_y-sen_y);
+  $("#z_val").text(st_z-sen_z);
 }
